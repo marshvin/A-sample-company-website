c45cf0df6fc8b74a90f6ce106a5f225fca135508
Vincent Marsh <91901144+marshvin@users.noreply.github.com>
2022-12-12 00:27:48 +0300

    Update info.py

--- gfg/info.py
+++ gfg/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'vincentmarsh01@gmail.'
-EMAIL_HOST_PASSWORD = 'vincent@5506'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
